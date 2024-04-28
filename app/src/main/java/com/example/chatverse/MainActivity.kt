692ca6c80c6cb880eb8b9ce36da34902d60a6dd0
MinalGhate <minalghate21@gmail.com>
2024-04-28 19:54:01 +0530

    Added login activity

--- app/src/main/java/com/example/chatverse/MainActivity.kt
+++ app/src/main/java/com/example/chatverse/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.chatverse
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginBtn: Button = findViewById(R.id.login_button)
+        loginBtn.setOnClickListener{
+            Log.d("MainActitvity", "Trying to show Login activity")
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+            Log.d("LoginActivity", "login page successfully shown ")
+
+        }
+
+
+
+
+
+
+
+            
+
+
+
     }
 }
